3b406e0b885b178912f4dbc8401bb68c75851fc9
Iliyan Ivanchev <157714078+iliyanivanchev@users.noreply.github.com>
2024-01-25 17:24:21 +0200

    Extract routes
    Add home controller

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const handlebars = require('express-handlebars');
 const path = require('path');
 
+const router = require('./routes');
+
 const app = express();
 const PORT = 5000;
 
@@ -13,8 +15,6 @@ app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', (req, res) => {
-    res.render('home', {layout: false});
-});
+app.use(router);
 
 app.listen(PORT, () => console.log(`Server is listening on port ${PORT}...`));
