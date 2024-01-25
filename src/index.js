60294093b484b49c3f862326d62e72bb1a15207a
Iliyan Ivanchev <157714078+iliyanivanchev@users.noreply.github.com>
2024-01-25 16:17:04 +0200

    Setup express

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-console.log('init');
+const express = require('express');
+
+const app = express();
+const PORT = 5000;
+
+app.get('/', (req, res) => {
+    res.send('Home page');
+});
+
+app.listen(PORT, () => console.log(`Server is listening on port ${PORT}...`));
