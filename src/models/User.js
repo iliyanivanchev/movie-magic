83b842f500a7743cd4b1e875c38ed0475cc39ce9
Iliyan Ivanchev <157714078+iliyanivanchev@users.noreply.github.com>
2024-02-10 11:04:34 +0200

    Hash password on save

--- src/models/User.js
+++ src/models/User.js
@@ -1,4 +1,5 @@
 const { Schema, model } = require('mongoose');
+const bcrypt = require('bcrypt');
 
 const userSchema = new Schema({
     email: {
@@ -11,6 +12,12 @@ const userSchema = new Schema({
         type: String,
         required: true
     }
+});
+
+userSchema.pre('save', async function() {
+    const hash = await bcrypt.hash(this.password, 12);
+    
+    this.password = hash;
 });
 
 const User = model('User', userSchema);
