db13e7ea5eda79410e1a86ea4f640e83c0b97b97
Iliyan Ivanchev <157714078+iliyanivanchev@users.noreply.github.com>
2024-02-10 15:11:08 +0200

    Add route guard

---
+++ src/config/config.js
@@ -0,0 +1,3 @@
+module.exports = {
+    SECRET: 'dfdlsgkmdlk432fdsafkln2lk3n4lkn5342emkflsk'
+};
--- src/controllers/castController.js
+++ src/controllers/castController.js
@@ -1,12 +1,13 @@
 const router = require('express').Router();
 
 const castService = require('../services/castService');
+const { isAuth } = require('../middlewares/authMiddleware');
 
-router.get('/create', (req, res) => {
+router.get('/create', isAuth, (req, res) => {
     res.render('cast/create');
 });
 
-router.post('/create', async (req, res) => {
+router.post('/create', isAuth, async (req, res) => {
     const castData = req.body;
 
     await castService.create(castData);
